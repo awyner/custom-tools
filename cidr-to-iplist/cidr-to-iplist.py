bbff2d00086791a7feb9810c1789102d28b88ab4
awyner <awyner.git@gmail.com>
2022-07-14 16:42:47 -0400

    Added cidr-to-iplist and wordlist-generator.sh

---
+++ cidr-to-iplist/cidr-to-iplist.py
@@ -0,0 +1,45 @@
+# Written by Alex Wyner
+# Simple script to convert CIDR ranges to a list of IPs
+
+from ipaddress import IPv4Network
+from sys import argv
+
+
+def read_file(filename):
+    ips = []
+    in_file = ''
+    try:
+        in_file = open(filename, encoding='utf-8')
+        for line in in_file:
+            line = line.strip()
+            for ip in IPv4Network(line, False):
+                ips.append(str(ip))
+    except FileNotFoundError:
+        print('[!] File not found')
+        exit(1)
+    finally:
+        if type(in_file) != str:
+            in_file.close()
+    return ips
+
+
+def write_file(filename, ip_list):
+    try:
+        out_file = open(filename, 'w', encoding='utf-8')
+        for ip in ip_list:
+            out_file.write(ip + "\n")
+    finally:
+        out_file.close()
+
+
+if __name__ == '__main__':
+    try:
+        assert '.txt' in argv[1]
+        ip_list = read_file(argv[1])
+        write_file(argv[2], ip_list)
+    except IndexError:
+        print('Usage: cidr-to-iplist input-file output-file')
+        exit(1)
+    except AssertionError:
+        print('[!] Input must be text file')
+        exit(1)
