77ede87a92ddae5afd1a91737808639d4f52dec5
awyner <alex.wyner@claconnect.com>
2023-06-08 13:28:47 -0400

    Changed filename for file generated by snapback-helper.sh to prevent confusion with files generated in danipt

--- snapback-helper.sh
+++ snapback-helper.sh
@@ -1,6 +1,6 @@
 #1/bin/bash
 
-if [ $1 == "-h" ] 
+if [[ $1 == "-h" ]] 
 then
     echo "Usage: snapback-helper.sh"
     echo "Call script from directory containing host.txt files."
@@ -9,27 +9,27 @@ fi
 
 if [ -f "http_80.txt" ]
 then
-    while read -r line; do echo 'http://'$line >> all-hosts.txt; done < http_80.txt
+    while read -r line; do echo 'http://'$line >> possible-webservers.txt; done < http_80.txt
 fi
 
 if [ -f "http_8080.txt" ] 
 then 
-    while read -r line; do echo 'http://'$line':8080' >> all-hosts.txt; done < http_8080.txt
+    while read -r line; do echo 'http://'$line':8080' >> possible-webservers.txt; done < http_8080.txt
 fi
 
 if [ -f "http_443.txt" ]
 then
-    while read -r line; do echo 'https://'$line >> all-hosts.txt; done < http_443.txt
+    while read -r line; do echo 'https://'$line >> possible-webservers.txt; done < http_443.txt
 fi
 
 if [ -f "http_8443.txt" ]
 then
-    while read -r line; do echo 'https://'$line':8443' >> all-hosts.txt; done < http_8443.txt
+    while read -r line; do echo 'https://'$line':8443' >> possible-webservers.txt; done < http_8443.txt
 fi
 
-if [ -f "all-hosts.txt" ]
+if [ -f "possible-webservers.txt" ]
 then
-    echo "Done. all-hosts.txt created"
+    echo "Done. possible-webservers.txt created"
 else
     echo "Done. No input files found"
 
