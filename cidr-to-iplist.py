9502108aa8f39bd814aa156aefba34b9038654d0
awyner <alex.wyner@claconnect.com>
2023-06-13 16:03:28 -0400

    Made output file optional

--- cidr-to-iplist.py
+++ cidr-to-iplist.py
@@ -3,7 +3,7 @@
 
 from ipaddress import IPv4Network
 from sys import argv
-
+import time
 
 def read_file(filename):
     ips = []
@@ -15,7 +15,7 @@ def read_file(filename):
             for ip in IPv4Network(line, False):
                 ips.append(str(ip))
     except FileNotFoundError:
-        print('[!] File not found')
+        print('[!] Error: File not found')
         exit(1)
     finally:
         if type(in_file) != str:
@@ -36,10 +36,13 @@ if __name__ == '__main__':
     try:
         assert '.txt' in argv[1]
         ip_list = read_file(argv[1])
-        write_file(argv[2], ip_list)
+        if len(argv) == 2:
+            write_file('ip-list-' + str(int(time.process_time()*(10**6))) + '.txt', ip_list)
+        else:
+            write_file(argv[2], ip_list)
     except IndexError:
-        print('Usage: cidr-to-iplist input-file output-file')
+        print('Usage: cidr-to-iplist input-file [output-file]')
         exit(1)
     except AssertionError:
-        print('[!] Input must be text file')
+        print('[!] Error: Input must be text file')
         exit(1)
